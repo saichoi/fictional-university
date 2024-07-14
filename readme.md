431a565d6034c79fc2812d7bb75c518e6ee4574a
saichoi <saichoi0424@gmail.com>
2024-07-15 06:52:31 +0900

    #4-11 Get Page and Post

--- readme.md
+++
@@ -1,9 +0,0 @@
-## 테마 초기 설정시 주의 점
-: 을 앞에 띄어쓰기 하지 말 것
-```
-/*
-    Theme Name: Fincional University
-    Author: Choi Dahye
-    Version: 1.0
-*/
-```
--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -1,8 +1,8 @@
 <?php 
     
-    // Wordpress POST 가져오는 방법
     while(have_posts()) {
         the_post(); ?>
+        <h1>This is Page, not a Post</h1>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_content(); ?>
         <hr>
