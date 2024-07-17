055e8e16d9120869bb152e81ed30cd4045a8da75
saichoi <saichoi0424@gmail.com>
2024-07-18 06:29:43 +0900

    #5-19 Menu of Child Page Links

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -29,15 +29,35 @@
                             </p>
                         </div>
                     <?php }
-                ?>
 
-                <!-- <div class="page-links">
-                    <h2 class="page-links__title"><a href="#">About Us</a></h2>
-                    <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
-                    </ul>
-                </div> -->
+                $testArray = get_pages(array(
+                    'child_of' => get_the_ID(),
+                ));
+
+                // 부모 페이지가 있거나 자식 페이지가 있는 경우 
+                if ($theParent or $testArray) {
+                ?>
+                    <div class="page-links">
+                        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
+                        <ul class="min-list">
+                            <?php 
+                                // 부모 페이지가 있다면, 즉 자식 페이지라면 부모 페이지의 자식들을 리스트에 표시하고
+                                if ($theParent) {
+                                    $findChildrenOf = $theParent;
+                                // 부모 페이지라면 해당 페이지 즉 부모 페이지의 자식들을 리스트에 표시한다.
+                                } else {
+                                    $findChildrenOf = get_the_ID();
+                                }
+                                wp_list_pages(array(
+                                    'title_li' => NULL, // 'Page' 라는 제목을 비표시하고 Page에 해당하는 자식을 가져온다.
+                                    'child_of' => $findChildrenOf,
+                                    'sort_culumn' => 'menu_order'
+                                ));
+                            ?>
+                        </ul>
+                    </div>
+
+                <?php } ?>
 
                 <div class="generic-content">
                     <?php the_content() ?>
