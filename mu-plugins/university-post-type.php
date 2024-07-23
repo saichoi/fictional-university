00b31d5af1de75d4002b590879710c149a77d1f0
saichoi <saichoi0424@gmail.com>
2024-07-24 07:15:09 +0900

    #7-30 Displaying Custom Post Types ( Event )

--- mu-plugins/university-post-type.php
+++ mu-plugins/university-post-type.php
@@ -1,9 +1,14 @@
 <?php
 // mu-plugins : 테마에 기본적으로 사용해야하는 기능(Custom Post Type)을 플러그인으로 등록할 수 있다.
+// Custom Post Type 신규 등록 시에 관리화면에서 setting > permalink를 업데이트해야 worpdress가 인식할 수 있다.
 
 // Event Post Type
 function university_post_types () {
     register_post_type('event', array(
+        'rewrite' => array(
+            'slug' => 'events',
+        ),
+        'has_archive' => true, // Custom Post Type의 archive 페이지를 사용함. (permalink 업데이트 해야함)
         'public' => true,
         'show_in_reset'=> true, // REST API에서 접근 가능하게 설정(최신 편집기 사용할 수 있음)
         'labels'=> array(
