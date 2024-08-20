44c860b82fa73b33403ab8541cbee703ef04c6b7
saichoi <saichoi0424@gmail.com>
2024-08-21 07:43:34 +0900

    #12-56. Campus Map on Front-End (JS)

--- themes/fictional-university-theme/src/index.js
+++ themes/fictional-university-theme/src/index.js
@@ -8,4 +8,4 @@ import GoogleMap from "./modules/GoogleMap"
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
-const googleMap = new GoogleMap()
+const googleMap = new GoogleMap() 
