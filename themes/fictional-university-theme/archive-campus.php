4954d614dd38a791657336067124c014b2fd3934
saichoi <saichoi0424@gmail.com>
2024-08-22 06:59:33 +0900

    #12-57. Campus Map Detail & Create Campus Single Page

--- themes/fictional-university-theme/archive-campus.php
+++ themes/fictional-university-theme/archive-campus.php
@@ -1,4 +1,4 @@
-<?php get_header(); 
+<div?php get_header(); 
 pageBanner(array(
     'title' => 'Our Campuses',
     'subtitle' => 'We have serveal conveniently located campuses.'
@@ -12,12 +12,15 @@ pageBanner(array(
         the_post(); 
         $mapLocation = get_field('map_location');
         ?>
-        <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>", data-lng="<?php echo $mapLocation['lng']; ?>"></div>
+        <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>", data-lng="<?php echo $mapLocation['lng']; ?>">
+            <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+            <?php echo $mapLocation['address']; ?>
+        </div>
     <?php endwhile; 
     
     echo paginate_links();
     ?>
-    </>
+    </div>
 </div>
 
 <?php get_footer(); ?>
