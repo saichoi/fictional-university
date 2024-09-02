62ff001765a3f25f4e992c4a184f8cd483dccf1a
saichoi <saichoi0424@gmail.com>
2024-09-03 07:00:14 +0900

    #13-64. Managing Time in JavaScript

--- themes/fictional-university-theme/build/index.asset.php
+++ themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '7357582ccc82e8953136');
+<?php return array('dependencies' => array('jquery'), 'version' => 'b390a6b07e130b3f6e98');
--- themes/fictional-university-theme/src/modules/Search.js
+++ themes/fictional-university-theme/src/modules/Search.js
@@ -6,21 +6,28 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.typingTimer;
     }
 
     // 2.  events
     events() {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
-        $(document).on("keydown", this.keyPressDispatcher.bind(this));
-
-        // keyup : 한번만 동작.
-        // keydown : 여러번 동작.
+        $(document).on("keydown", this.keyPressDispatcher.bind(this)); // keyup : 한번만 동작. keydown : 여러번 동작.
+        this.searchField.on("keydown", this.typingLogic.bind(this));
     }
 
     // 3. methods (function, action ...)
+    typingLogic() { 
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(() => {
+            console.log("This is a timeout test.")
+        }, 2000);
+    }
+
     keyPressDispatcher(e) {
         if (e.keyCode == 83 && !this.isOverlayOpen) {
             this.openOverlay();
