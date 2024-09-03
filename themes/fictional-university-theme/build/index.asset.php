5c9716aa543034004d71e697ae44f3ffe369c21a
saichoi <saichoi0424@gmail.com>
2024-09-04 07:00:03 +0900

    #13-65. Waiting / Loading Spinner Icon

--- themes/fictional-university-theme/build/index.asset.php
+++ themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'b390a6b07e130b3f6e98');
+<?php return array('dependencies' => array('jquery'), 'version' => 'f8ffbbafe23944cd0a45');
