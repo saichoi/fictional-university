97b6606c381e2b0492c2b24ac8774cfd71b8d8fd
saichoi <saichoi0424@gmail.com>
2024-09-27 07:12:02 +0900

    #18-89. Open Registration (Part2)

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -133,3 +133,25 @@ function noSubsAdminBar() {
         show_admin_bar(false);
     endif;
 }
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+    wp_enqueue_style("custom-google-fonts", '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i'); // fonts
+    wp_enqueue_style("font-awesome", '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); // icon
+    wp_enqueue_style("university_main_styles", get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style("university_extra_styles", get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+    return get_bloginfo('name');
+}
