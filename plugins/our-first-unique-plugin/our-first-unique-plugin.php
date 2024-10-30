eeb2ad54a3d9964c9c1e3c50116779a7f7b3dd13
saichoi <saichoi0424@gmail.com>
2024-10-31 05:49:16 +0900

    #23-127. Finishing Our Settings Form

--- plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -16,14 +16,58 @@ class WordCountAndTImePlugin {
 
     function settings() {
         add_settings_section('wcp_first_section', NULL, NULL, 'word-count-settings-page');
+
         add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => 0));
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => 0));
+
+        add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
+
+        add_settings_field('wcp_wordcount', 'Word Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_wordcount'));
+        register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        add_settings_field('wcp_charactercount', 'Character Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_charactercount'));
+        register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_readtime'));
+        register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
     }
 
+    function sanitizeLocation($input) {
+        if ($input !== '0' AND $input != '1') {
+            add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end.');
+            return get_option('wcp_location');
+        }
+        return $input;
+    }
+
+    /*
+    function readTimeHTML() { ?>
+        <input type="checkbox" name="wcp_readtime" value="1" <?php checked(get_option('wcp_readtime'), 1) ?>">
+    <?php }
+
+    function charactercountHTML() { ?>
+        <input type="checkbox" name="wcp_charactercount" value="1" <?php checked(get_option('wcp_charactercount'), '1') ?>>
+    <?php }
+
+    function wordcountHTML() { ?>
+        <input type="checkbox" name="wcp_wordcount" value="1" <?php checked(get_option('wcp_wordcount'), 1) ?>">
+    <?php }
+    */
+
+    // reusable checkbox function
+    function checkboxHTML($args) { ?>
+        <input type="checkbox" name="<?php echo $args['theName'] ?>" value="1" <?php checked(get_option($args['theName']), '1') ?>>
+    <?php }
+
+    function headlineHTML() { ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')) ?>">
+    <?php }
+
     function locationHTML() { ?>
-        <select name="wcp_loaction">
-            <option value="0">Beginning of post</option>
-            <option value="1">End of post</option>
+        <select name="wcp_location">
+            <option value="0" <?php selected(get_option('wcp_location'), '0') ?>>Beginning of post</option>
+            <option value="1" <?php selected(get_option('wcp_location'), '1') ?>>End of post</option>
         </select>
     <?php }
 
