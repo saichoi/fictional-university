19056fbf29c89e8f68d0ac5e2016c282a01d363a
saichoi <saichoi0424@gmail.com>
2024-11-07 04:58:16 +0900

    #23-131. Custom Admin Menu Icon

--- plugins/our-word-filter-plugin/index.php
+++ plugins/our-word-filter-plugin/index.php
@@ -16,13 +16,28 @@ class OurWordFilterPlugin {
     }
 
     function ourMenu() {
-        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+        // add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), plugin_dir_url(__FILE__) . 'custom.svg', 100);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', 100);
         add_submenu_page('ourwordfilter', 'Word To Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'));
         add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage'));
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+    }
+
+    function mainPageAssets() {
+        wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__). 'style.css');
     }
 
     function wordFilterPage() { ?>
-        Hello World.
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <form method="POST">
+                <label for="plugin_words_to_filter"><p>Enter a <strong>comma-senparated</strong> list of word to filter from your site's content.</p></label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"></textarea>
+                </div>
+            </form>
+            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+        </div>
     <?php }
 
 function optionsSubPage() { ?>
