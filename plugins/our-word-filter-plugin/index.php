20312ae0a743e9c2d39a4955bf707176a5f4deff
saichoi <saichoi0424@gmail.com>
2024-11-12 04:47:39 +0900

    #23-134. Finishing Word Filter Plugin

--- plugins/our-word-filter-plugin/index.php
+++ plugins/our-word-filter-plugin/index.php
@@ -13,6 +13,25 @@ if ( ! defined('ABSPATH') ) exit; // Exit if accessed directly
 class OurWordFilterPlugin {
     function __construct() {
         add_action('admin_menu', array($this, 'ourMenu'));
+        add_action('admin_init', array($this, 'ourSettings'));
+        if (get_option('plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic')); 
+    }
+
+    function ourSettings() {
+        add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+        register_setting('replacementFields', 'replacementText');
+        add_settings_field('replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+    }
+
+    function replacementFieldHTML() { ?>
+        <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '****')); ?>">
+        <p class="description">Leave blan to simply remove the fitlerd words.</p>
+    <?php }
+
+    function filterLogic($content) {
+        $badWords = explode(',', get_option('plugin_words_to_filter'));
+        $badWordsTrimmed = array_map('trim', $badWords);
+        return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', '****')), $content);
     }
 
     function ourMenu() {
@@ -58,7 +77,17 @@ class OurWordFilterPlugin {
     <?php }
 
 function optionsSubPage() { ?>
-    Hello World fron the options page.
+    <div class="wrap">
+        <h1>Word Filter Options</h1>
+        <form action="options.php" method="POST">
+            <?php
+                settings_errors();
+                settings_fields('replacementFields');
+                do_settings_sections('word-filter-options');
+                submit_button();
+            ?>
+        </form>
+    </div>
 <?php }
 }
 
